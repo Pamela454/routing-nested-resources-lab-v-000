d1a670c951d09ed9170e314dc1ac922cdea5e7c5
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 19:06:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Author.find(params[:author_id]).posts
+      @songs = Artist.find(params[:author_id]).posts
     else
       @posts = Post.all
     end
