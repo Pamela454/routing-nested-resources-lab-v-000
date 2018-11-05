1e153f2c4831d30fdc4f22d0a5729449e2412914
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 19:06:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @songs = Artist.find(params[:artist_id]).posts
     else
-      @posts = Post.all
+      @songs = Post.all
     end
   end
 
