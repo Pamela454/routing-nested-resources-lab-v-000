4c5c7e11f50877965398093f58503d32e5d51d24
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 19:26:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,7 @@ class SongsController < ApplicationController
   def show
     if @song = Song.find(params[:id])
     else
+      flash[:alert] = "Artist not found."
       redirect_to artist_songs
     end
   end
